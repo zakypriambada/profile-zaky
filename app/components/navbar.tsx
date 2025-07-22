86092ff7dc1763c78881608f5a2b95ac755044da
ZakyPriambada <zakypriambada05@gmail.com>
2025-07-23 00:57:55 +0700

    update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -33,20 +33,20 @@ const Navbar: React.FC<NavbarProps> = ({ items }) => {
 
     return (
         <nav
-            className={`md:bg-blue-900/7 md:backdrop-blur-md bg-white/90 py-[10px] w-full border-b border-neutral-300 transition-all duration-800 ease-in-out transform fixed top-0 left-0 right-0 z-50 ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
+            className={`md:bg-blue-900/7 md:backdrop-blur-md bg-white/85 py-[10px] w-full border-b border-neutral-300 transition-all duration-800 ease-in-out transform fixed top-0 left-0 right-0 z-50 ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
                 }`}
         >
             <div className="max-w-screen-xl mx-auto px-6 flex justify-center items-center">
-                <ul className="hidden md:flex space-x-8 ">
+                <ul className="hidden md:flex space-x-8">
                     {items.map((item, index) => (
-                        <li key={index} className="relative group">
+                        <li key={index} className="relative group flex items-center h-full">
                             {item.toLowerCase() === 'home' && pathname === '/' ? (
                                 <button
                                     onClick={handleHomeClick}
-                                    className="relative cursor-pointer text-[16px] text-black text-lg transition-all duration-300 group bg-transparent border-none focus:outline-none"
+                                    className="relative cursor-pointer text-[16px]  text-black text-lg transition-all duration-300 group"
                                 >
                                     {item}
-                                    <span className="absolute left-1/2 bottom-0 w-0 h-[1px] bg-black transform transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                                    <span className="absolute left-1/2 bottom-[-10] w-0 h-[1px] bg-black transform transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
                                 </button>
                             ) : (
                                 <Link
@@ -54,7 +54,7 @@ const Navbar: React.FC<NavbarProps> = ({ items }) => {
                                     className="relative text-[16px] cursor-pointer text-black text-lg transition-all duration-300 group"
                                 >
                                     {item}
-                                    <span className="absolute left-1/2 bottom-0 w-0 h-[1px] bg-black transform transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                                    <span className="absolute left-1/2 bottom-[-10] w-0 h-[0.9px] bg-black transform transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
                                 </Link>
                             )}
                         </li>
@@ -68,7 +68,7 @@ const Navbar: React.FC<NavbarProps> = ({ items }) => {
             </div>
 
             <ul
-                className={`md:hidden flex flex-col bg-white/90 space-y-4 p-4 absolute top-14 left-0 w-full transition-all duration-300 ${isMenuOpen ? 'block' : 'hidden'
+                className={`md:hidden flex flex-col bg-white/85 space-y-4 p-4 absolute top-14 left-0 w-full transition-all duration-300 ${isMenuOpen ? 'block' : 'hidden'
                     }`}
             >
                 {items.map((item, index) => (
