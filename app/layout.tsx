5aecd43a931521716f6fdc694c422c37e8cfcbdc
ZakyPriambada <zakypriambada05@gmail.com>
2025-08-10 20:52:51 +0700

    update cv

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// 1. Impor font Geist dari paketnya langsung, bukan dari 'next/font/google'
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
